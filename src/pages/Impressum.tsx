5f8688e0778fe9e58fcc13416c8c6f5c8c6d742c
Arian Glander <arian.glander@Mac.fritz.box>
2025-03-17 21:51:29 +0100

    Impressum

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -13,33 +13,33 @@ const Impressum = () => {
       <div className="container mx-auto px-4 py-12">
         <h1 className="text-4xl font-bold mb-8 text-center">Impressum</h1>
         <div className="max-w-3xl mx-auto prose prose-lg">
-          <h2>Angaben gemäß § 5 TMG</h2>
+          <h2 className="mt-8 mb-2 text-xl">Angaben gemäß § 5 TMG</h2>
           <p>
             Freie Schule Woltersdorf e.V.<br />
             Vogelsdorfer Straße 106<br />
             15569 Woltersdorf
           </p>
 
-          <h2>Vertreten durch</h2>
+          <h2 className="mt-8 mb-2 text-xl">Vertreten durch</h2>
           <p>
             Vorstandsvorsitzende: Susann Tröger<br />
             Geschäftsführer: Johannes Wilk
           </p>
 
-          <h2>Kontakt</h2>
+          <h2 className="mt-8 mb-2 text-xl">Kontakt</h2>
           <p>
             Telefon: 03362 700973<br />
             E-Mail: info@freie-schule-woltersdorf.de
           </p>
 
-          <h2>Registereintrag</h2>
+          <h2 className="mt-8 mb-2 text-xl">Registereintrag</h2>
           <p>
             Eintragung im Vereinsregister Frankfurt (Oder)<br />
             Registergericht: Amtsgericht Frankfurt (Oder)<br />
             Registernummer: VR 5179 FF
           </p>
 
-          <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+          <h2 className="mt-8 mb-2 text-xl">Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
           <p>
             Freie Schule Woltersdorf e.V.<br />
             Vogelsdorfer Straße 106<br />
